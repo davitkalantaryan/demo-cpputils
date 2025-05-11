706658b0a2ca386c7ae414b88270c0fe09b09954
Davit Kalantaryan <davit.kalantaryan@desy.de>
2025-05-11 10:52:37 +0200

    added project to demonstrated shared_ptr problem to delete friends

---
+++ src/tests/main_std_shared_ptr_friend_error.cpp
@@ -0,0 +1,28 @@
+//
+// repo:			cpputilsdemo
+// file:            main_std_shared_ptr_friend_error.cpp
+// path:			src/tests/main_std_shared_ptr_friend_error.cpp
+// created on:		2025 May 05
+// created by:		Davit Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+
+#include <memory>
+
+class TestClass
+{
+    friend class ::std::shared_ptr<TestClass>;
+    ~TestClass() = default;
+public:
+    TestClass() = default;
+};
+
+int main(int a_argc, char* a_argv[])
+{
+    static_cast<void>(a_argc);
+    static_cast<void>(a_argv);
+
+    ::std::shared_ptr<TestClass> smrtPtr(new TestClass());
+
+    return 0;
+}
