a3f6bd1be986a5954e79bb79fc86fff1c61849a8
Davit Kalantaryan <davit.kalantaryan@desy.de>
2025-05-11 11:11:54 +0200

    added workspace file to handle repo files

---
+++ src/utils/entry_cpputilsdemo_all.cpp
@@ -0,0 +1,17 @@
+//
+// repo:			cpputilsdemo
+// file:            entry_cpputilsdemo_all.cpp
+// path:			src/utils/entry_cpputilsdemo_all.cpp
+// created on:		2025 May 05
+// created by:		Davit Kalantaryan (davit.kalantaryan@desy.de)
+//
+
+#include <cpputilsdemo/internal_header.h>
+
+CPPUTILS_BEGIN_C
+
+CPPUTILS_DLL_PUBLIC void dummy_cpputilsdemo_all(void)
+{
+}
+
+CPPUTILS_END_C
